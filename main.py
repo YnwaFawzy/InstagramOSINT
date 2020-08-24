51ff565e739d32b95965acb294be749b1d885765
YnwaFawzy <38886749+YnwaFawzy@users.noreply.github.com>
2020-08-24 09:56:16 +0400

    Fix for user not found issue #12

--- main.py
+++ main.py
@@ -6,6 +6,7 @@
 # This is the main script meant to be run from the command line
 #
 #
+from pprint import pprint
 import argparse
 from bs4 import BeautifulSoup
 import json
@@ -33,65 +34,86 @@ class colors:
 class InstagramOSINT:
 
     def __init__(self, username, downloadPhotos):
-        self.useragents = ['Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36',
-                 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36',
-                 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36',
-                 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/602.2.14 (KHTML, like Gecko) Version/10.0.1 Safari/602.2.14',
-                 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36',
-                 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.98 Safari/537.36',
-                 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.98 Safari/537.36',
-                 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36',
-                 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36',
-                 'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:50.0) Gecko/20100101 Firefox/50.0']
-                 
+        self.useragents = [
+            'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36',
+            'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36',
+            'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36',
+            'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/602.2.14 (KHTML, like Gecko) Version/10.0.1 Safari/602.2.14',
+            'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36',
+            'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.98 Safari/537.36',
+            'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.98 Safari/537.36',
+            'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36',
+            'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36',
+            'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:50.0) Gecko/20100101 Firefox/50.0']
+
         self.username = username
         # Make the directory that we are putting the files into
         self.make_directory()
         print(colors.OKGREEN + f"[*] Starting Scan on {self.username}" + colors.ENDC)
         # Get the html data with the requests module
-        r = requests.get(f'http://instagram.com/{self.username}', headers={'User-Agent': random.choice(self.useragents)})
-        soup = BeautifulSoup(r.text, 'html.parser')
+        r = requests.get(f'http://instagram.com/{self.username}',
+                         headers={'User-Agent': random.choice(self.useragents)})
+
+        soup = BeautifulSoup(r.text, features="html.parser")
         # To prevent a unicode error, we need the following line...
         soup.encode('utf-8')
+
         # Find the tags that hold the data we want to parse
         general_data = soup.find_all('meta', attrs={'property': 'og:description'})
         more_data = soup.find_all('script', attrs={'type': 'text/javascript'})
         description = soup.find('script', attrs={'type': 'application/ld+json'})
-        # Try to parse the content -- if it fails then the program exits
-        try:
-            self.text = general_data[0].get('content').split()
-            # This is the profile description data
-            self.description = json.loads(description.get_text())
-            # This is the javascript json that is passed into json.loads()
-            self.profile_meta = json.loads(more_data[3].get_text()[21:].strip(';'))
 
-        except:
+        # Try to parse the content -- if it fails then the program exits
+
+        try:
+
+            self.text = general_data[0].get('content').split()
+
+            # This is the profile description data
+            # exit("hello")
+            if (description is not None):
+                self.description = json.loads(description.string)
+
+            # This is the javascript json that is passed into json.loads()
+            self.profile_meta = json.loads(more_data[3].string[21:].strip(';'))
+
+
+        except Exception as e:
+
+            pprint(description)
+
+            print(e)
             print(colors.FAIL + f"Username {self.username} not found" + colors.ENDC)
             sys.exit()
-        self.profile_data = {"Username": self.profile_meta['entry_data']['ProfilePage'][0]['graphql']['user']['username'],
-                             "Profile name": self.description['name'],
-                             "URL": self.description['mainEntityofPage']['@id'],
-                             "Followers": self.text[0], "Following": self.text[2], "Posts": self.text[4],
-                             "Bio": str(
-                                 self.profile_meta['entry_data']['ProfilePage'][0]['graphql']['user']['biography']),
-                             "profile_pic_url": str(self.profile_meta['entry_data']['ProfilePage'][0]['graphql']['user'][
-                                                        'profile_pic_url_hd']),
-                             "is_business_account": str(
-                                 self.profile_meta['entry_data']['ProfilePage'][0]['graphql']['user'][
-                                     'is_business_account']),
-                             "connected_to_fb": str(self.profile_meta['entry_data']['ProfilePage'][0]['graphql']['user'][
-                                                        'connected_fb_page']),
-                             "externalurl": str(
-                                 self.profile_meta['entry_data']['ProfilePage'][0]['graphql']['user']['external_url']),
-                             "joined_recently": str(self.profile_meta['entry_data']['ProfilePage'][0]['graphql']['user'][
-                                                        'is_joined_recently']),
-                             "business_category_name": str(
-                                 self.profile_meta['entry_data']['ProfilePage'][0]['graphql']['user'][
-                                     'business_category_name']),
-                             "is_private": str(
-                                 self.profile_meta['entry_data']['ProfilePage'][0]['graphql']['user']['is_private']),
-                             "is_verified": str(
-                                 self.profile_meta['entry_data']['ProfilePage'][0]['graphql']['user']['is_verified'])}
+        self.profile_data = {
+            "Username": self.profile_meta['entry_data']['ProfilePage'][0]['graphql']['user']['username'],
+            "Followers": self.text[0], "Following": self.text[2], "Posts": self.text[4],
+            "Bio": str(
+                self.profile_meta['entry_data']['ProfilePage'][0]['graphql']['user']['biography']),
+            "profile_pic_url": str(self.profile_meta['entry_data']['ProfilePage'][0]['graphql']['user'][
+                                       'profile_pic_url_hd']),
+            "is_business_account": str(
+                self.profile_meta['entry_data']['ProfilePage'][0]['graphql']['user'][
+                    'is_business_account']),
+            "connected_to_fb": str(self.profile_meta['entry_data']['ProfilePage'][0]['graphql']['user'][
+                                       'connected_fb_page']),
+            "externalurl": str(
+                self.profile_meta['entry_data']['ProfilePage'][0]['graphql']['user']['external_url']),
+            "joined_recently": str(self.profile_meta['entry_data']['ProfilePage'][0]['graphql']['user'][
+                                       'is_joined_recently']),
+            "business_category_name": str(
+                self.profile_meta['entry_data']['ProfilePage'][0]['graphql']['user'][
+                    'business_category_name']),
+            "is_private": str(
+                self.profile_meta['entry_data']['ProfilePage'][0]['graphql']['user']['is_private']),
+            "is_verified": str(
+                self.profile_meta['entry_data']['ProfilePage'][0]['graphql']['user']['is_verified'])}
+
+        if (description is not None):
+            {
+                "Profile name": self.description['name'],
+                "URL": self.description['mainEntityofPage']['@id'],
+            }
 
         # Tries to scrape posts if it is a public profile
         self.save_data()
@@ -109,7 +131,8 @@ class InstagramOSINT:
         else:
             print("[*]Getting Photos")
             posts = {}
-            for index, post in enumerate(self.profile_meta['entry_data']['ProfilePage'][0]['graphql']['user']['edge_owner_to_timeline_media']['edges']):
+            for index, post in enumerate(self.profile_meta['entry_data']['ProfilePage'][0]['graphql']['user'][
+                                             'edge_owner_to_timeline_media']['edges']):
                 os.mkdir(str(index))
                 try:
                     posts[index] = {"Caption": str(post['node']['edge_media_to_caption']['edges'][0]['node']['text']),
@@ -132,10 +155,13 @@ class InstagramOSINT:
 
                 # Downloads the thumbnails of the post
                 # Picture is just an int index of the url in the list
-                with open(f'{os.getcwd()}/{index}/' + ''.join([random.choice(string.ascii_uppercase) for x in range(random.randint(1, 9))]) + '.jpg', 'wb') as f:
+                with open(f'{os.getcwd()}/{index}/' + ''.join(
+                        [random.choice(string.ascii_uppercase) for x in range(random.randint(1, 9))]) + '.jpg',
+                          'wb') as f:
                     # Delay the request times randomly (be nice to Instagram)
                     time.sleep(random.randint(5, 10))
-                    r = requests.get(post['node']['thumbnail_resources'][0]['src'], headers={'User-Agent':random.choice(self.useragents)})
+                    r = requests.get(post['node']['thumbnail_resources'][0]['src'],
+                                     headers={'User-Agent': random.choice(self.useragents)})
                     # Takes the content of r and puts it into the file
                     f.write(r.content)
                     print("Got an Image")
@@ -191,14 +217,16 @@ class InstagramOSINT:
         """
         with open("profile_pic.jpg", "wb") as f:
             time.sleep(1)
-            r = requests.get(self.profile_data['profile_pic_url'], headers={'User-Agent':random.choice(self.useragents)})
+            r = requests.get(self.profile_data['profile_pic_url'],
+                             headers={'User-Agent': random.choice(self.useragents)})
             f.write(r.content)
 
 
 def parse_args():
     parser = argparse.ArgumentParser(description="Instagram OSINT tool")
     parser.add_argument("--username", help="profile username", required=True, nargs=1)
-    parser.add_argument("--downloadPhotos", help="Downloads the users photos if their account is public", required=False, action='store_true')
+    parser.add_argument("--downloadPhotos", help="Downloads the users photos if their account is public",
+                        required=False, action='store_true')
     return parser.parse_args()
 
 
